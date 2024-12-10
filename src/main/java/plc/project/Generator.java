21bdf814b54455cbc58e23fb883638cef6ea4595
justin22a <justin.adam@gmail.com>
2024-12-09 20:29:36 -0500

    fixing generator

--- src/main/java/plc/project/Generator.java
+++ src/main/java/plc/project/Generator.java
@@ -1,10 +1,12 @@
-package plc.project;
 
+
+package plc.project;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Objects;
 
 public final class Generator implements Ast.Visitor<Void> {
-
     private final PrintWriter writer;
     private int indent = 0;
     private boolean generatingIncrement = false;
@@ -12,7 +14,6 @@ public final class Generator implements Ast.Visitor<Void> {
     public Generator(PrintWriter writer) {
         this.writer = writer;
     }
-
     private void print(Object... objects) {
         for (Object object : objects) {
             if (object instanceof Ast) {
@@ -22,12 +23,59 @@ public final class Generator implements Ast.Visitor<Void> {
             }
         }
     }
-
     private void newline(int indent) {
         writer.println();
         for (int i = 0; i < indent; i++) {
-            writer.write("    ");
+            writer.write(" ");
         }
+    }
+    private String changeName(String name){
+        if (Objects.equals(name, "Integer")){
+            return "int";
+        }else if (Objects.equals(name, "Boolean")){
+            return "boolean";
+        }else if (Objects.equals(name, "Decimal")){
+            return "double";
+        }
+        return name;
+    }
+    @Override
+    public Void visit(Ast.Source ast) {
+        writer.write("public class Main {");
+        indent += 4;
+        if (!ast.getFields().isEmpty()) {
+            newline(0);
+            for (Ast.Field field : ast.getFields()) {
+                newline(indent);
+                writer.write(field.toString());
+            }
+        }
+        if (!ast.getMethods().isEmpty()) {
+            newline(0);
+        }
+        newline(indent);
+        writer.write("public static void main(String[] args) {");
+        newline(indent += 4);
+
+        writer.write("System.exit(new Main().main());");
+        newline(indent -= 4);
+        writer.write("}");
+        if (!ast.getMethods().isEmpty()) {
+            for (Ast.Method method : ast.getMethods()) {
+                newline(0);
+                newline(indent);
+
+                print(method);
+            }
+        }
+        indent -= 2;
+        indent -= 2;
+        newline(0);
+        newline(indent);
+
+        writer.write("}");
+
+        return null;
     }
 
     private String mapTypeName(String typeName) {
@@ -39,52 +87,66 @@ public final class Generator implements Ast.Visitor<Void> {
             default: return typeName;
         }
     }
-
-    @Override
-    public Void visit(Ast.Source ast) {
-        writer.write("public class Main {");
-        newline(0);
-        indent++;
-
-        // Fields
-        for (Ast.Field field : ast.getFields()) {
-            newline(indent);
-            visit(field);
-        }
-
-        if (!ast.getFields().isEmpty() && !ast.getMethods().isEmpty()) {
-            newline(0);
-        }
-
-        // Main method
-        newline(indent);
-        writer.write("public static void main(String[] args) {");
-        indent++;
-        newline(indent);
-        writer.write("System.exit(new Main().main());");
-        indent--;
-        newline(indent);
-        writer.write("}");
-
-        // Methods
-        for (Ast.Method method : ast.getMethods()) {
-            newline(0);
-            newline(indent);
-            visit(method);
-        }
-
-        indent--;
-        newline(0);
-        writer.write("}");
-        return null;
-    }
-
     @Override
     public Void visit(Ast.Field ast) {
         if (ast.getConstant()) {
             writer.write("final ");
         }
+
         writer.write(mapTypeName(ast.getTypeName()) + " " + ast.getName());
+
+        if (ast.getValue().isPresent()) {
+            writer.write(" = ");
+            visit(ast.getValue().get());
+        }
+
+        writer.write(";");
+        return null;
+    }
+    @Override
+    public Void visit(Ast.Method ast) {
+        String mType = changeName(ast.getFunction().getType().getJvmName());
+        writer.write(mType);
+        writer.write(" ");
+        writer.write(ast.getName());
+        writer.write("(");
+        if (!ast.getParameters().isEmpty()) {
+            for (int i = 0; i < ast.getParameters().size(); i++) {
+                if (i > 0) {
+                    writer.write(", ");
+                }
+                String paramType = changeName(ast.getParameterTypeNames().get(i));
+                writer.write(paramType);
+                writer.write(" ");
+                writer.write(ast.getParameters().get(i));
+            }
+        }
+        writer.write(") {");
+        if (ast.getStatements().isEmpty()) {
+            writer.write("}");
+        }
+        else
+        {
+            for (Ast.Statement statement : ast.getStatements()) {
+                newline(indent += 4);
+                print(statement);
+                indent -= 4;
+            }
+            newline(indent);
+            writer.write("}");
+        }
+
+        return null;
+    }
+    @Override
+    public Void visit(Ast.Statement.Expression ast) {
+        visit(ast.getExpression());
+        writer.write(";");
+        return null;
+    }
+    @Override
+    public Void visit(Ast.Statement.Declaration ast) {
+        writer.write(mapTypeName(ast.getVariable().getType().getJvmName()) + " " + ast.getName());
         if (ast.getValue().isPresent()) {
             writer.write(" = ");
             visit(ast.getValue().get());
@@ -92,17 +154,100 @@ public final class Generator implements Ast.Visitor<Void> {
         writer.write(";");
         return null;
     }
+    @Override
+    public Void visit(Ast.Statement.Assignment ast) {
+        visit(ast.getReceiver());
+        writer.write(" = ");
+        visit(ast.getValue());
+        if (!generatingIncrement) {
+            writer.write(";");
+        }
+        return null;
+    }
+    @Override
+    public Void visit(Ast.Statement.If ast){
+        boolean temp = ast.getThenStatements().isEmpty();
+
+        if (temp){
+            writer.write("if (");
+            writer.write(ast.getCondition().toString());
+
+            writer.write(") {}");
+        }else{
+            writer.write("if (");
+
+            print(ast.getCondition());
+            writer.write(") {");
+
+            for (int i = 0; i < ast.getThenStatements().size(); i++){
+                newline(indent+=4);
+
+                print(ast.getThenStatements().get(i));
+                indent-=4;
+            }
+            temp = !ast.getElseStatements().isEmpty();
+            if (temp){
+                newline(0);
+                writer.write("} else {");
+
+                for (int i = 0; i < ast.getElseStatements().size(); i++){
+                    newline(indent+=4);
+                    print(ast.getElseStatements().get(i));
+                    indent-=4;
+                }
+            }
+            newline(indent);
+            writer.write("}");
+        }
+        return null;
+    }
+
 
     @Override
-    public Void visit(Ast.Method ast) {
-        writer.write(mapTypeName(ast.getFunction().getReturnType().getJvmName()) + " " + ast.getName() + "(");
-        for (int i = 0; i < ast.getParameters().size(); i++) {
-            writer.write(mapTypeName(ast.getFunction().getParameterTypes().get(i).getJvmName()) + " " + ast.getParameters().get(i));
-            if (i < ast.getParameters().size() - 1) {
-                writer.write(", ");
-            }
+    public Void visit(Ast.Statement.For ast){
+        writer.write("for ( ");
+
+        if (ast.getInitialization() != null) {
+            print(ast.getInitialization());
+        }else{
+
+            writer.write(";");
         }
+        writer.write(" ");
+        print(ast.getCondition());
+        writer.write(";");
+
+        if (ast.getIncrement() != null){
+            generatingIncrement = true;
+            writer.write(" ");
+            print(ast.getIncrement());
+            generatingIncrement = false;
+        }
+        writer.write(" ) ");
+        boolean temp = ast.getStatements().isEmpty();
+        if (temp) {
+            writer.write("{}");
+
+        } else {
+            writer.write("{");
+            for (Ast.Statement statement : ast.getStatements()) {
+                newline(indent+=4);
+
+                print(statement);
+
+                indent-=4;
+            }
+            newline(indent);
+            writer.write("}");
+        }
+        return null;}
+
+    @Override
+    public Void visit(Ast.Statement.While ast) {
+        writer.write("while (");
+        visit(ast.getCondition());
         writer.write(") {");
+
         if (ast.getStatements().isEmpty()) {
             writer.write("}");
         } else {
@@ -117,107 +262,6 @@ public final class Generator implements Ast.Visitor<Void> {
         }
         return null;
     }
-
-    @Override
-    public Void visit(Ast.Statement.Expression ast) {
-        visit(ast.getExpression());
-        writer.write(";");
-        return null;
-    }
-
-    @Override
-    public Void visit(Ast.Statement.Declaration ast) {
-        writer.write(mapTypeName(ast.getVariable().getType().getJvmName()) + " " + ast.getName());
-        if (ast.getValue().isPresent()) {
-            writer.write(" = ");
-            visit(ast.getValue().get());
-        }
-        writer.write(";");
-        return null;
-    }
-
-    @Override
-    public Void visit(Ast.Statement.Assignment ast) {
-        visit(ast.getReceiver());
-        writer.write(" = ");
-        visit(ast.getValue());
-        if (!generatingIncrement) {
-            writer.write(";");
-        }
-        return null;
-    }
-
-    @Override
-    public Void visit(Ast.Statement.If ast) {
-        writer.write("if (");
-        visit(ast.getCondition());
-        writer.write(") {");
-        for (Ast.Statement statement : ast.getThenStatements()) {
-            indent++;
-            newline(indent);
-            visit(statement);
-            indent--;
-        }
-        newline(indent);
-        writer.write("}");
-        if (!ast.getElseStatements().isEmpty()) {
-            writer.write(" else {");
-            for (Ast.Statement statement : ast.getElseStatements()) {
-                indent++;
-                newline(indent);
-                visit(statement);
-                indent--;
-            }
-            newline(indent);
-            writer.write("}");
-        }
-        return null;
-    }
-
-    @Override
-    public Void visit(Ast.Statement.For ast) {
-        writer.write("for (");
-        if (ast.getInitialization() != null) {
-            visit(ast.getInitialization());
-        } else {
-            writer.write(";");
-        }
-        writer.write(" ");
-        visit(ast.getCondition());
-        writer.write("; ");
-        if (ast.getIncrement() != null) {
-            generatingIncrement = true;
-            visit(ast.getIncrement());
-            generatingIncrement = false;
-        }
-        writer.write(") {");
-        for (Ast.Statement statement : ast.getStatements()) {
-            indent++;
-            newline(indent);
-            visit(statement);
-            indent--;
-        }
-        newline(indent);
-        writer.write("}");
-        return null;
-    }
-
-    @Override
-    public Void visit(Ast.Statement.While ast) {
-        writer.write("while (");
-        visit(ast.getCondition());
-        writer.write(") {");
-        for (Ast.Statement statement : ast.getStatements()) {
-            indent++;
-            newline(indent);
-            visit(statement);
-            indent--;
-        }
-        newline(indent);
-        writer.write("}");
-        return null;
-    }
-
     @Override
     public Void visit(Ast.Statement.Return ast) {
         writer.write("return ");
@@ -225,24 +269,34 @@ public final class Generator implements Ast.Visitor<Void> {
         writer.write(";");
         return null;
     }
-
     @Override
-    public Void visit(Ast.Expression.Literal ast) {
-        Object literal = ast.getLiteral();
-        if (literal == null) {
-            writer.write("null");
-        } else if (literal instanceof Character) {
-            writer.write("'" + literal.toString().replace("'", "\\'") + "'");
-        } else if (literal instanceof String) {
-            writer.write("\"" + literal.toString().replace("\"", "\\\"") + "\"");
-        } else if (literal instanceof BigDecimal) {
-            writer.write(((BigDecimal) literal).toPlainString());
-        } else {
-            writer.write(literal.toString());
+    public Void visit(Ast.Expression.Literal ast){
+        boolean tempc = ast.getType() == Environment.Type.CHARACTER;
+        if(tempc){
+            writer.write("'");
+            writer.write(ast.getLiteral().toString());
+            writer.write("'");
+        }
+        else if (ast.getType() == Environment.Type.STRING){
+            writer.write("\"");
+
+            writer.write(ast.getLiteral().toString());
+            writer.write("\"");
+        }
+        else if (ast.getType() == Environment.Type.DECIMAL){
+
+            print(((BigDecimal) ast.getLiteral()).toString());
+
+        }
+        else if (ast.getType() == Environment.Type.INTEGER){
+            print(((BigInteger) ast.getLiteral()).toString());
+
+        }else
+        {
+            writer.write(ast.getLiteral().toString());
         }
         return null;
     }
-
     @Override
     public Void visit(Ast.Expression.Group ast) {
         writer.write("(");
@@ -250,7 +304,6 @@ public final class Generator implements Ast.Visitor<Void> {
         writer.write(")");
         return null;
     }
-
     @Override
     public Void visit(Ast.Expression.Binary ast) {
         visit(ast.getLeft());
@@ -258,7 +311,6 @@ public final class Generator implements Ast.Visitor<Void> {
         visit(ast.getRight());
         return null;
     }
-
     @Override
     public Void visit(Ast.Expression.Access ast) {
         if (ast.getReceiver().isPresent()) {
@@ -268,7 +320,6 @@ public final class Generator implements Ast.Visitor<Void> {
         writer.write(ast.getVariable().getJvmName());
         return null;
     }
-
     @Override
     public Void visit(Ast.Expression.Function ast) {
         if (ast.getReceiver().isPresent()) {
